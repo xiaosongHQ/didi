d9053a3bc12cdaa5465fd0b341d999c04a15e79a
Jayshi <240638896@qq.com>
2018-12-27 12:53:09 +0800

    27-12:53

--- didi/src/router/index.js
+++ didi/src/router/index.js
@@ -21,7 +21,7 @@ Vue.use(VueResource);
 
 // 数据请求
 import Axios from 'axios'
-Axios.defaults.baseURL = 'http://jc.zonguo.cn:6060';
+Axios.defaults.baseURL = 'http://129.204.110.142:8080';
 // Axios.defaults.baseURL = '/api';
 Vue.prototype.$ajax = Axios;
 
